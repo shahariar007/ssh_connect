683114e09845922eab544ef8ecc065976ec5ba83
mortuza <mortuza@squaregroup.com>
2023-01-05 17:11:04 +0600

    master one

--- app/src/main/java/com/ms/timerforedtl/MainActivity2.java
+++ app/src/main/java/com/ms/timerforedtl/MainActivity2.java
@@ -51,7 +51,6 @@ public class MainActivity2 extends AppCompatActivity {
     String PASSWORD_KEY = "PASSWORD";
     String USERID_KEY = "USERID";
 
-// done gg
     String item[] = {
             "uptime -p",
             "sudo shutdown now",
