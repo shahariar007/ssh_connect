1d5aff62f11dbbf90f494318f41e3e528f500f7d
mortuza <mortuza@squaregroup.com>
2023-01-09 13:36:03 +0600

    hellod

--- app/src/main/java/com/ms/timerforedtl/MainActivity2.java
+++ app/src/main/java/com/ms/timerforedtl/MainActivity2.java
@@ -61,7 +61,7 @@ public class MainActivity2 extends AppCompatActivity {
             "ping",
             "sudo netstat -tunlp"
     };
-// abc
+// abcdf
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
