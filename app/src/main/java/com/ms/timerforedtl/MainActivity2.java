e38e8601ef6175f3209f03c824ce5bb91336639a
mortuza <mortuza@squaregroup.com>
2023-01-09 10:35:33 +0600

    first commit

--- app/src/main/java/com/ms/timerforedtl/MainActivity2.java
+++ app/src/main/java/com/ms/timerforedtl/MainActivity2.java
@@ -46,7 +46,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     String USERID = "pi";
 // hope
-
+// hola madrid
     String HOST_KEY = "HOST";
     String PASSWORD_KEY = "PASSWORD";
     String USERID_KEY = "USERID";
