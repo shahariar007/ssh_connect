e28f04db27073592ae337cd38c566a2b728b5280
mortuza <mortuza@squaregroup.com>
2023-01-09 12:51:46 +0600

    dd

--- app/src/main/java/com/ms/timerforedtl/MainActivity2.java
+++ app/src/main/java/com/ms/timerforedtl/MainActivity2.java
@@ -45,10 +45,7 @@ public class MainActivity2 extends AppCompatActivity {
     ImageView settings;
 
     String USERID = "pi";
-// hope
-// hola madrid
-    //fdsfds
-    // asdf
+
     String HOST_KEY = "HOST";
     String PASSWORD_KEY = "PASSWORD";
     String USERID_KEY = "USERID";
