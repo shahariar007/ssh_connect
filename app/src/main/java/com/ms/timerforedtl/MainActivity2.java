799f93fab42adb72fb3d36b6588443f31946626a
mortuza <mortuza@squaregroup.com>
2023-01-09 12:40:31 +0600

    last lovedsa

--- app/src/main/java/com/ms/timerforedtl/MainActivity2.java
+++ app/src/main/java/com/ms/timerforedtl/MainActivity2.java
@@ -48,6 +48,7 @@ public class MainActivity2 extends AppCompatActivity {
 // hope
 // hola madrid
     //fdsfds
+    //dada ds
     String HOST_KEY = "HOST";
     String PASSWORD_KEY = "PASSWORD";
     String USERID_KEY = "USERID";
